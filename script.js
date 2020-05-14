5c8abe5f9460860cc948fbc064bfbee369a35bc9
Restyled.io <commits@restyled.io>
2020-05-14 16:01:04 +0000

    Restyled by prettier

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ const openPopup = document.querySelector("#pop");
 const openButton = document.querySelector(".openPopup");
 const closeButton = document.querySelector(".close");
 
-openButton.addEventListener("click",
-                            () => { openPopup.style.display = "block"; });
+openButton.addEventListener("click", () => {
+  openPopup.style.display = "block";
+});
 
-closeButton.addEventListener("click",
-                             () => { openPopup.style.display = "none"; });
+closeButton.addEventListener("click", () => {
+  openPopup.style.display = "none";
+});
